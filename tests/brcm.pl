924b1eba79dea50310c22fd80ccdf45ce276ad97
kcudnik <kcudnik@gmail.com>
2018-11-14 18:54:56 +0100

    Add unittests for warm boot

--- tests/brcm.pl
+++ tests/brcm.pl
@@ -301,7 +301,72 @@ sub test_brcm_buffer_pool
     play "full_buffer_second.rec",0;
 }
 
+sub test_brcm_warm_boot_full
+{
+    fresh_start;
+
+    play "full.rec";
+
+    request_warm_shutdown;
+    start_syncd_warm;
+
+    play "full_second.rec";
+
+    request_warm_shutdown;
+}
+
+sub test_brcm_warm_boot_empty
+{
+    fresh_start;
+
+    play "empty_sw.rec";
+
+    request_warm_shutdown;
+    start_syncd_warm;
+
+    play "empty_sw.rec", 0;
+
+    request_warm_shutdown;
+}
+
+sub test_brcm_warm_boot_small_buffer
+{
+    fresh_start;
+
+    play "small_buffer.rec";
+
+    request_warm_shutdown;
+    start_syncd_warm;
+
+    play "small_buffer.rec", 0;
+
+    request_warm_shutdown;
+}
+
+sub test_brcm_warm_boot_full_empty
+{
+    fresh_start;
+
+    play "full.rec";
+
+    request_warm_shutdown;
+    start_syncd_warm;
+
+    play "empty_sw.rec";
+    play "empty_sw.rec", 0;
+    play "full_second.rec";
+    play "empty_sw.rec";
+
+    request_warm_shutdown;
+}
+
 # RUN TESTS
+
+test_brcm_warm_boot_full_empty;
+test_brcm_warm_boot_small_buffer;
+test_brcm_warm_boot_empty;
+test_brcm_warm_boot_full;
+
 test_brcm_buffer_pool;
 test_brcm_acl_tables;
 test_brcm_qos_map_order;
